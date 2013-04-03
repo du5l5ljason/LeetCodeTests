8def9225031859887f37781da213c467bd17133f
du5l5ljason <tfang.du@gmail.com>
2013-04-03 15:40:37 -0700

    Consider all the zeros, and check if we can skip 0s, if we can, then we can jump to the last Ids.

--- JumpGame.cpp
+++ JumpGame.cpp
@@ -7,21 +7,58 @@
 //
 
 #include <iostream>
+#include <string>
 
+using namespace std;
 class Solution {
 public:
+    int calcMaxJump( int A[], int n, int startId, int endId )
+    {
+        int range=0;
+        if( startId == endId ) 
+            return 0;
+        else
+        {
+            for( int i = startId; i< endId; ++i )
+            {
+                range = max( range, i+A[i] );
+            }
+        }
+        return range;
+    }
     
     bool canJump(int A[], int n) {
         // Start typing your C/C++ solution below
-        if( n== 0 ) return false;
+        if( n == 0 ) return false;
         if( n == 1 ) return true;
         
-        int range = A[0];
-        for( int i = 0; i<=range; ++i )
+        std::vector<int> zeroIds;
+        for(int i = 0 ; i<n ; i++ )
         {
-            range = max( range, i+A[i] );
-            if( range >=n-1 ) return true;
+            if( A[i] == 0 )
+                zeroIds.push_back(i);
         }
-        return false;
+        int len = zeroIds.size();
+        if(len == 0 )
+            return true;
+        else
+        {
+            int last0 = 0;
+            int maxRange[len];
+            int range = 0;
+            for(int i=0;i<len;i++)
+            {
+                maxRange[i] = calcMaxJump( A, n, last0, zeroIds[i] );
+                range = max( maxRange[i] , range );
+                if( range >= n-1 )
+                    return true;
+                else
+                {
+                    last0 = zeroIds[i];
+                    j++;
+                }
+            }
+            return false;
+        }
     }
 };
