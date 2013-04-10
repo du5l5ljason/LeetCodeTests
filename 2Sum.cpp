4889111133c1923da51cb65826d82647addc35b8
du5l5ljason <tfang.du@gmail.com>
2013-04-10 13:49:49 -0700

    use a map, don't know how to use hash map in C++, but it works

--- 2Sum.cpp
+++ 2Sum.cpp
@@ -14,47 +14,40 @@ public:
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
         vector<int> result;
-        vector<int> numbersCopy = numbers;
-        sort( numbers.begin(), numbers.end() );
+        
+        map<int, int> map;
         
         int len = numbers.size();
-        int id1 = 0;
-        int id2 = len-1;
-        int min, max;
-        
-        while( id1 < id2 )
+        int l1 = 0;
+        int l2 = 0;
+        if( len < 2 ) return result;
+        else
         {
-            if( numbers[id1] + numbers[id2] < target )
-                id1++;
-            else if( numbers[id1] + numbers[id2] > target )
-                id2--;
-            else
+            for( int i  = 0 ; i< numbers.size(); i++ )
             {
-                min = numbers[id1];
-                max = numbers[id2];
-                break;
+                map[i] = target - numbers[i];
             }
+            
+            for( int i = 0 ; i<numbers.size() ; i++ )
+            {
+                for( int j = 0 ; j < numbers.size(); j++ )
+                {
+                    if( map[i] == numbers[j] && i!= j )
+                    {
+                        l1 = j;
+                        l2 = i;
+                    }
+                }
+            }
+            
+            if( l1 > l2 )
+                swap( l1 , l2 );
+            
+            result.push_back( l1+1 );
+            result.push_back( l2+1 );
+            return result;
         }
         
-        for( int i = 0 ; i < numbersCopy.size() ; ++i )
-        {
-            if( numbersCopy[i] == min )
-                min = i+1;
-        }
-        
-        for( int i = 0 ; i < numbersCopy.size() ; ++i )
-        {
-            if( numbersCopy[i] == max && i!=min-1)
-                max = i+1;
-        }
-        
-        if( min > max )
-            swap( min, max );
-        
-        result.push_back( min );
-        result.push_back( max );
-        
-        return result;
         
     }
 };
