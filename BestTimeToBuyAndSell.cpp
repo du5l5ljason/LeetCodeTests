ce87f5c6e630e88997b7e6833c57196afed82e50
du5l5ljason <tfang.du@gmail.com>
2013-07-07 15:54:48 -0700

    Comments Added.

--- BestTimeToBuyAndSell.cpp
+++ BestTimeToBuyAndSell.cpp
@@ -5,40 +5,34 @@
 //  Created by 廷芳 杜 on 4/26/13.
 //  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 //
-
-#include <iostream>
+//  From the question, we know:
+//  1. for buying, we only want to buy at those days that the price is "local minimum".
+//  Further, if day j is after day i, then we only consider to modify our strategy at day j if price[j] is lower than price[i].
+//  2. Keep updating the largest profit, if it is larger than current profit, save that day to the sell day.
 
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        
-        int min, maxProfit, buyDay, sellDay,curProfit;
-        
-        buyDay = 0;
-        sellDay = 0;
-        min = 0;
-        maxProfit = 0;
-        
         if( prices.size() == 0 ) return 0;
-        if( prices.size() == 1 ) return 0;
         
-        min = prices[0];
-        for( int i = 0 ; i < prices.size(); i++ )
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        int curProfit = 0;
+
+        for( int i = 0; i < prices.size(); i++ )
         {
-            if( prices[i] < min )
-            {
-                min = prices[i];
-                buyDay = i;
-            }    
-            curProfit = prices[i] - min;
-            
+            //Keep updating the largest profit
+            curProfit = prices[i] - minPrice;
             if( curProfit > maxProfit )
             {
                 maxProfit = curProfit;
-                sellDay = i;
             }
+            
+            //update the minimum value.
+            if( prices[i] < minPrice )
+                minPrice = prices[i];
         }
         
         return maxProfit;
