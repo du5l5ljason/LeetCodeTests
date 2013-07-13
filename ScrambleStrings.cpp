47b7d6175d654aae0230ac0d901e406be1c53c9b
du5l5ljason <tfang.du@gmail.com>
2013-07-13 16:37:43 -0700

    Alternative Solution: DP. Has bugs: Run Status: Wrong Answer Progress: 36/48 test cases passed.
    "aabc", "abad", "aabadabc"	false	true
    
    "aabc", "abad", "aabaabdc"	false	true
    
    "aabc", "abad", "aabcabad"	true	true
    
    "aabc", "abad", "aabadabc"	false	true
    
    "aabc", "abad", "aabacbad"	false	true

--- ScrambleStrings.cpp
+++ ScrambleStrings.cpp
@@ -10,43 +10,38 @@
 
 class Solution {
 public:
-    bool isInterleaveHelper( string s1, int id1, string s2, int id2, string s3, int id3 )
-    {
-        if( id3 < 0 ) return true;
-        if( id1 < 0 && id2 < 0 ) return false;
-        if( id1 < 0 )
-        {
-            if( s3[id3] == s2[id2] )
-                return isInterleaveHelper( s1, id1, s2, id2-1, s3, id3-1 );
-            else
-                return false;
-        }
-        else if( id2 < 0 )
-        {
-            if( s3[id3] == s1[id1] )
-                return isInterleaveHelper( s1, id1-1, s2, id2, s3, id3-1 );
-            else
-                return false;            
-        }
-        else
-        {
-            if( s3[id3] != s1[id1] && s3[id3] != s2[id2] ) return false;
-            if( s3[id3] != s1[id1] )
-                return isInterleaveHelper( s1, id1, s2, id2-1, s3, id3-1 );
-            if( s3[id3] != s2[id2] )
-                return isInterleaveHelper( s1, id1-1, s2, id2, s3, id3-1 );
-            
-            return isInterleaveHelper( s1, id1-1, s2, id2, s3, id3-1 ) || isInterleaveHelper( s1, id1, s2, id2-1, s3, id3-1 );
-        }
-    }
     bool isInterleave(string s1, string s2, string s3) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function    
-        if( s3.size() != s1.size()+s2.size() ) return false;
-        int id1 = s1.size()-1;
-        int id2 = s2.size()-1;
-        int id3 = s3.size()-1;
+        if( s1.empty() && s2.empty() && s3.empty() ) return true;
+        if( s3.empty() ) return false;
+        if( s1.empty() ) return s2==s3?true:false;
+        if( s2.empty() ) return s1==s3?true:false;
         
-        return isInterleaveHelper( s1, id1, s2, id2, s3, id3 );
+        if( s3.size() != s1.size()+s2.size() ) return false;
+        bool isValid[s1.size()][s2.size()];
+        
+        if( s3[0]!=s1[0]&&s3[0]!=s2[0] ) return false;
+        isValid[0][0] = true;
+        for( int i = 1; i < s1.size(); i++ )
+            isValid[i][0] = (s3[i]==s1[i]?true:false);
+        for( int i = 1; i < s2.size(); i++ )
+            isValid[0][i] = (s3[i]==s2[i]?true:false);
+        for( int i = 1; i < s1.size(); i++ )
+        {
+            for( int j = 1; j<s2.size(); j++ )
+            {
+                if( (s3[i+j+1] == s1[i] ) && s3[i+j+1] == s2[j])
+                    isValid[i][j] = isValid[i-1][j] || isValid[i][j-1];
+                else if( s3[i+j+1] == s2[j] )
+                    isValid[i][j] = isValid[i][j-1];
+                else if(s3[i+j+1] == s1[i] )
+                    isValid[i][j] = isValid[i-1][j];
+                else
+                    isValid[i][j] = false;
+            }
+        }
+        
+        return isValid[s1.size()-1][s2.size()-1];
     }
 };
