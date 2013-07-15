fd4037c154414dd20b2c00a07c03a33ee08e4f52
du5l5ljason <tfang.du@gmail.com>
2013-07-15 15:14:27 -0700

    no message

--- WordLadderII.cpp
+++ WordLadderII.cpp
@@ -7,7 +7,6 @@
 //
 
 #include <iostream>
-
 class Solution {
 public:
     struct pathNode{
@@ -18,14 +17,14 @@ public:
             pre = NULL;
         };
     };
-    void findPath( pathNode &node, vector<string> &path )
+    void findPath( pathNode *node, vector<string> &path )
     {
         while( node!=NULL )
         {
-            path.push_back( node.str );
+            path.push_back( node->str );
             node = node->pre;
         }
-        swap( path.begin(), path.end() );
+        reverse( path.begin(), path.end() );
     }
     vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
         // Start typing your C/C++ solution below
@@ -40,24 +39,32 @@ public:
             return pathPool;
         }
         
-        queue<pathNode> u;
-        u.push( pathNode(start) );
-        u.push( pathNode("") );
+        queue<pathNode*> u;
+        u.push( new pathNode(start) );
+        u.push( new pathNode("") );
         bool isFound = false;
         while( !u.empty() )
         {
-            pathNode v = u.front();
+            pathNode *v = u.front();
             u.pop();
-            if( isFound && v.str=="") return pathPool;
+            if( isFound && v->str=="") return pathPool;
             else
             {
-                if( v.str=="" ) //reach to the end of a level
+                if( v->str=="" ) //reach to the end of a level
                 {
-                    if( !u.empty() ) u.push( pathNode("") );
+                    //delete words in the queue
+                    queue<pathNode*> temp = u;
+                    while( !temp.empty() )
+                    {
+                        pathNode *pt = temp.front();
+                        temp.pop();
+                        dict.erase( pt->str );
+                    }
+                    if( !u.empty() ) u.push( new pathNode("") );
                 }
                 else
                 {
-                    if( v.str == end )
+                    if( v->str == end )
                     {
                         vector<string> vec;
                         findPath( v,vec );
@@ -66,26 +73,27 @@ public:
                     }
                     else
                     {
-                        string temp = v.str;
-                        for( int i = 0; i < v.str.length(); i++ )
+                        string temp = v->str;
+                        for( int i = 0; i < v->str.length(); i++ )
                         {
                             for( char j = 'a' ; j <= 'z'; j++ )
                             {
-                                v.str[i] = j;
-                                if( v.str == temp ) continue;
+                                v->str[i] = j;
+                                if( v->str == temp ) continue;
                                 else 
                                 {
-                                    if( dict.find( v.str ) !=dict.end() )
+                                    if( dict.count( v->str ) >0 )
                                     {
-                                        pathNode newNode(v);
-                                        newNode->pre = &v;
+                                        pathNode *newNode = new pathNode(v->str);
+                                        newNode->pre = v;
                                         u.push( newNode );
+                                        //dict.erase( newNode->str );
                                     }
                                 }
                             }
-                            v.str = temp;
+                            v->str = temp;
                         }
-                        dict.erase( v.str );
+                        //dict.erase( v->str );
                     }
                 }
                 
